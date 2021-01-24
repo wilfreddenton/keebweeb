ee0dde40d1ace0fe467b9d0aeb563ca8364021bd
wilfreddenton <dentonw3@gmail.com>
2021-01-24 03:13:38 -0500

    big refactor

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+function CGroup() {
+  const cGroup = document.createElement('span')
+  cGroup.classList.add('c-group')
+  return cGroup
+}
+
 class CC { // stands for Controlled Character
-  constructor(c) {
+  constructor(c, group) {
     const span = document.createElement('span')
     c = this._transform(c)
     span.innerHTML = c
     span.classList.add('cc')
+    group.appendChild(span)
 
     this._char = c
+    this._group = group
     this._element = span
   }
 
   _transform(c) {
     if (c === " ") return "&nbsp;"
     return c
-  }
-
-  appendElementTo(parent) {
-    parent.appendChild(this._element)
   }
 
   revert() {
@@ -33,95 +37,105 @@ class CC { // stands for Controlled Character
   }
 
   insertBefore(element) {
-    this._element.parentNode.insertBefore(element, this._element)
+    this._group.insertBefore(element, this._element)
   }
 
   insertAfter(element) {
-    this._element.parentNode.appendChild(element)
+    this._group.insertBefore(element, this._element.nextSibling)
   }
 }
 
-function CGroup() {
-  const cGroup = document.createElement('span')
-  cGroup.classList.add('c-group')
-  return cGroup
-}
+class TextBox {
+  constructor(id, text) {
+    this._text = text
+    this._box = document.getElementById(id)
+    this._cursor = document.createElement('span')
+    this._cursorIntervalParams = [() => this._cursor.classList.toggle('hide'), 530]
+    this._cursorInterval = null
+    this._typingTimeout = null
+    this._ccs = []
+    this._index = 0
 
-function Cursor() {
-  const outerSpan = document.createElement('span')
-  const innerSpan = document.createElement('span')
-  outerSpan.appendChild(innerSpan)
-  outerSpan.id = "cursor"
-  return outerSpan
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const text = `The sky above the port was the color of television, tuned to a dead channel.`
-  const elements = {
-    leader: document.getElementById('leader'),
-    cursor: new Cursor(),
-  }
-  const cursorIntervalParams = [() => elements.cursor.classList.toggle('hide'), 530]
-  const state = {
-    leaderChars: [],
-    index: 0,
-    typingTimeout: null,
-    cursorInterval: null
+    this._render()
+    this._setupListeners()
+    this.focus()
   }
 
-  const frag = document.createDocumentFragment();
-  let cGroup = null
-  text.split("").forEach(c => {
-    if (cGroup === null) cGroup = CGroup()
-    const cc = new CC(c)
-    cc.appendElementTo(cGroup)
-    frag.appendChild(cGroup)
-    state.leaderChars.push(cc)
-    if (c === " ") cGroup = null
-  })
-  elements.leader.appendChild(frag)
-  state.leaderChars[state.index].insertBefore(elements.cursor)
+  _render() {
+    this._cursor.appendChild(document.createElement('span'))
+    this._cursor.id = "cursor"
+    this._cursor.classList.add('focused')
 
-  state.cursorInterval = setInterval(...cursorIntervalParams)
-  document.addEventListener('click', e => {
-    elements.leader.classList.remove('focused')
-    clearInterval(state.cursorInterval)
-    elements.cursor.classList.add('hide')
-  })
-  leader.addEventListener('click', e => {
-    e.stopPropagation()
-    elements.leader.classList.add('focused')
-    elements.cursor.classList.remove('hide')
-    clearInterval(state.cursorInterval)
-    state.cursorInterval = setInterval(...cursorIntervalParams)
-  })
+    const frag = document.createDocumentFragment();
+    let cGroup = null
+    this._text.split("").forEach(c => {
+      if (cGroup === null) cGroup = CGroup()
+      const cc = new CC(c, cGroup)
+      frag.appendChild(cGroup)
+      this._ccs.push(cc)
+      if (c === " ") cGroup = null
+    })
+    this._box.appendChild(frag)
+    this._ccs[this._index].insertBefore(this._cursor)
+  }
 
-  document.addEventListener('keydown', e => {
-    if ([e.altKey, e.ctrlKey, e.metaKey].some(b => b)) return
-    if (!leader.classList.contains('focused')) return
-    if (state.index >= text.length) return
+  _setupListeners() {
+    document.addEventListener('click', this.unFocus)
+    this._box.addEventListener('click', e => {
+      e.stopPropagation()
+      this.focus()
+    })
+    document.addEventListener('keydown', e => {
+      if ([e.altKey, e.ctrlKey, e.metaKey].some(b => b)) return
+      this.input(e.key)
+    })
+  }
 
-    clearInterval(state.cursorInterval)
-    clearTimeout(state.typingTimeout)
-    elements.cursor.classList.remove('hide')
-    let cc = null
-    if (e.key === "Backspace") {
-      if (state.index < 1) return
-      cc = state.leaderChars[state.index-1]
+  focus() {
+    this._box.classList.add('focused')
+    this._cursor.classList.remove('hide')
+    clearInterval(this._cursorInterval)
+    this._cursorInterval = setInterval(...this._cursorIntervalParams)
+  }
+
+  unFocus() {
+    this._box.classList.remove('focused')
+    clearInterval(this._cursorInterval)
+    this._cursor.classList.add('hide')
+  }
+
+  input(key) {
+    if (!this._box.classList.contains('focused')) return
+    if (this._index >= this._text.length) return
+
+    clearInterval(this._cursorInterval)
+    clearTimeout(this._typingTimeout)
+    this._cursor.classList.remove('hide')
+
+    if (key === "Backspace") {
+      if (this._index < 1) return
+      const cc = this._ccs[this._index-1]
       cc.insertBefore(cursor)
       cc.revert()
-      state.index -= 1
+      this._index -= 1
     } else {
-      if (!/^.$/.test(e.key)) return
-      cc = state.leaderChars[state.index]
-      cc.validate(e.key)
-      state.index += 1
-      if (state.index < text.length) {
-        state.leaderChars[state.index].insertBefore(cursor)
+      if (!/^.$/.test(key)) return
+      const cc = this._ccs[this._index]
+      cc.validate(key)
+      this._index += 1
+      if (this._index < this._text.length) {
+        this._ccs[this._index].insertBefore(this._cursor)
       } else {
-        cc.insertAfter(cursor)
+        cc.insertAfter(this._cursor)
       }
     }
-    state.cursorInterval = setInterval(...cursorIntervalParams)
-  })
-})
+
+    this._cursorInterval = setInterval(...this._cursorIntervalParams)
+  }
+}
+
+function main() {
+  new TextBox('leader', `The sky above the port was the color of television, tuned to a dead channel.`)
+}
+
+document.addEventListener('DOMContentLoaded', () => main())
